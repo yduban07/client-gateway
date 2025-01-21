65b3ab290144ee85730d5039b8c5b2ee82fc0bd2
yduban07 <yduban27@gmail.com>
2025-01-20 20:11:12 -0600

    added signup logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,23 @@
 import { Post, Body, Get, Inject } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { NATS_SERVICE } from 'src/config';
 import { SigninUserDto, SignupUserDto } from './dto';
+import { firstValueFrom } from 'rxjs';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) { }
 
   @Post('signup')
-  registerUser(@Body() signupUserDto: SignupUserDto) {
-    return this.client.send('auth.signup.user', signupUserDto);
+  async registerUser(@Body() signupUserDto: SignupUserDto) {
+    try {
+
+      return await firstValueFrom(this.client.send('auth.signup.user', signupUserDto));
+      
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
   @Post('signin')
